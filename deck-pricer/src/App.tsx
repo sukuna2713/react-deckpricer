a236c76f12d6282b3473b2ee96dd2dd77f07ef80
sukuna2713 <sukuna2713@gmail.com>
2021-06-08 07:05:31 +0000

    on tutrial 1.5

--- deck-pricer/src/App.tsx
+++ deck-pricer/src/App.tsx
@@ -76,47 +76,66 @@ class Summary extends React.Component {
   }
 }
 
-class AdmissionFeeCalculator extends React.Component {
-  private details: DetailProps[] = [
-    {
-      classification: {
-        name: "adult",
-        description: "",
-        unitPrice: 1000,
-        numOfPeople: 0,
-        totalPrice: 0
-      }
-    },
-    {
-      classification: {
-        name: "children",
-        description: "under 18 years old",
-        unitPrice: 500,
-        numOfPeople: 0,
-        totalPrice: 0
-      }
-    },
-    {
-      classification: {
-        name: "baby",
-        description: "0 to 2",
-        unitPrice: 0,
-        numOfPeople: 0,
-        totalPrice: 0
-      }
-    }
-  ];
+class AdmissionFeeCalculator extends React.Component<{}, AdmissionFeeCalculatorState> {
+  constructor(props: {}) {
+    super(props);
+    const adults: FeeClassification = {
+      name: "大人",
+      description: "",
+      unitPrice: 1000,
+      numOfPeople: 0,
+      totalPrice: 0,
+    };
+    const students: FeeClassification = {
+      name: "学生",
+      description: "中学生・高校生",
+      unitPrice: 700,
+      numOfPeople: 0,
+      totalPrice: 0,
+    };
+    const children: FeeClassification = {
+      name: "子ども",
+      description: "小学生",
+      unitPrice: 300,
+      numOfPeople: 0,
+      totalPrice: 0,
+    };
+    const infants: FeeClassification = {
+      name: "幼児",
+      description: "未就学",
+      unitPrice: 0,
+      numOfPeople: 0,
+      totalPrice: 0,
+    };
+    this.state = { feeClassifications: [adults, students, children, infants] };
+  }
+
+  handleNumOfPeopleChange(idx: number, num: number) {
+    const currentFC = this.state.feeClassifications[idx];
+    const newTotalPrice = currentFC.unitPrice * num;
+    //変更しない部分のコピー
+    const newFC: FeeClassification = 
+      Object.assign({}, currentFC, { numOfPeople: num, totalPrice: newTotalPrice });
+    // 新たな配列を生成
+    const feeClassifications = this.state.feeClassifications.slice();
+    feeClassifications[idx] = newFC;
+
+    // stateの更新
+    this.setState({ feeClassifications: feeClassifications });
+  }
 
   render() {
-    const detailJsx = this.details.map((fc,idx) => {
-      return(
-        <Detail key={idx.toString()} classification={fc.classification} />
+    //TO DO show https://qiita.com/yonetty/items/012be4c5c6258a609e35#appendix-%E3%83%AD%E3%83%BC%E3%82%AB%E3%83%AB%E9%96%8B%E7%99%BA%E7%92%B0%E5%A2%83%E3%81%AE%E3%82%BB%E3%83%83%E3%83%88%E3%82%A2%E3%83%83%E3%83%97
+    const details = this.state.feeClassifications.map((fc,idx) => {
+      return (
+        <Detail key={idx.toString()} classification={fc}
+        onNumOfPeopleChange={n => this.handleNumOfPeopleChange(idx,n)} />
       );
     });
 
     return (
       <>
-        {detailJsx}
+        {details}
         <Summary />
       </>
     );
